b2a36f470aa871c11404e4b55e28a738c7e62fa2
Amir Hossein Partovi <a.partovi99@gmail.com>
2025-05-14 17:18:17 +0330

    BasicExceptions

--- BasicExceptions/BasicExceptions.cpp
+++ BasicExceptions/BasicExceptions.cpp
@@ -1,8 +1,41 @@
-// name BasicExeceptions.cpp
+// name Basicexceptions.cpp
 #include <iostream>
 using namespace std;
 
-int main(){
 
+void mightGoWrong(){
+    bool error1 = false;
+    bool error2 = false;
+    bool error3 = true;
+    
+    if(error1){
+        throw 8;
+    }
+    if(error2){
+        throw "Something went wrong";
+    }
+    if(error3){
+        throw string("Something bad went wrong");
+    }
+}
+void useMightGoWrong(){
+    //nested function error handling
+    mightGoWrong();
+}
+
+int main(){
+    try{
+        useMightGoWrong();
+    }
+    catch(int e){
+        cout << "Error code: " << e << endl;
+    }
+    catch(char const * e){
+        cout << "Error message: " << e << endl;
+    }
+    catch(string &e){
+        cout << "String Error message : "<< e << endl;
+    }
+    cout << "still runnig ..." << endl;
     return 0;
 }
---
+++ BasicExceptions/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+    catch(...){
+        cout << "Unknown error" << endl;
+    }
